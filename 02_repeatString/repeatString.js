e24b0fac52e044864480af8e9a4d213d4fc77470
Chris <chrisdnielson@gmail.com>
2022-09-25 16:46:51 -0700

    Complete exercise 02
    Completed the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    //takes an input string, str
+    //and returns a result string which is the input string
+    //repeated num times.
+    let result = [];
 
+    //input testing conditions
+    if(num < 0) {
+        return "ERROR";
+    }
+
+    for(let i = 0; i < num; i++) {
+        result.push(str);
+    }
+    
+    return result.join(""); //converts the string array into a string with no separator
 };
 
 // Do not edit below this line
