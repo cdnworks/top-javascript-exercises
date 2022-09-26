b76cebca28a69b10cf96dda9f27f765da36b94a3
Chris <chrisdnielson@gmail.com>
2022-09-26 11:13:18 -0700

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //leap years are divisible by 4, 
+    //and not divisible by 100, but are divisible by 400
+    //should return a bool
+    return (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0); 
 };
 
 // Do not edit below this line
