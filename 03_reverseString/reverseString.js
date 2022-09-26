7a280d520501a3a64cb421972e81ce87347381b2
Chris <chrisdnielson@gmail.com>
2022-09-25 17:20:42 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    //split str into an array
+    //return reversed array to string
+    let arr = str.split(""); //split by each char
+    return arr.reverse().join("");
 };
 
 // Do not edit below this line
