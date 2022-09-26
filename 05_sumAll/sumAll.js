6a838ba642fd0dedce308e8cf39f04a3e8bcbced
Chris <chrisdnielson@gmail.com>
2022-09-25 18:58:23 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //this function takes 2 integers and returns the sum of all
+    //integers between and including them.
 
+    //typecheck
+    if((typeof a != "number" && typeof a != "bigint") ||
+        (typeof b != "number" && typeof b != "bigint")) {
+        return "ERROR";
+    }
+
+    if(a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    let start, end;
+    if(a < b) {
+        start = a;
+        end = b;
+    }
+    else {
+        start = b;
+        end = a;
+    }
+
+    //calculate sum
+    let sum = 0;
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
