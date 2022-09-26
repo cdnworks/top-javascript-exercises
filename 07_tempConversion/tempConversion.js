36b4ff886a5961dbe27c26355e518907455c159a
Chris <chrisdnielson@gmail.com>
2022-09-26 11:42:17 -0700

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  //converts a number, f, in fahrenheit to c, celsius
+  c = (f - 32) * (5/9);
+  return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  //converts a number, c, in celsius to f, fahrenheit
+  f = c * (9/5) + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
