f7a34a69e6decb35ed3d1e1a86ab58617ae93ea5
Chris <chrisdnielson@gmail.com>
2022-09-25 18:31:52 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    //args should always be in the form:
+    //(array to modify, thing to remove 1, thing to remove 2, ..., etc.)
 
+    let arr =  args[0];     //copy original array
+    let result = [];        //array to contain the formatted array
+
+    arr.forEach(element => {            //iterate through each array item
+        if(!args.includes(element)) {   //if the element is NOT in the args list
+            result.push(element);       //add it to the result array
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line
