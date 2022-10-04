f4235e1ff2e50662549193b8c18e764e1d6a6f3c
Chris <chrisdnielson@gmail.com>
2022-10-04 14:28:58 -0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //remove any formatting, spaces and puctuation
+    //probably dont even have to remove spaces but whatever
+    //make a deep copy of the input, sanitize it as described above, then see if it's the same forward and backward
+    sanitizedString = str.toLowerCase().replace(/[^a-z]/g, "");
 
+    return (
+        sanitizedString.split("").reverse().join("") == sanitizedString //see if a reversed string (split back into an array, reversed and rejoined) is the same as the processed string
+    );
 };
 
 // Do not edit below this line
