d8e454a31815423432cf431ca82efd0238639805
Chris <chrisdnielson@gmail.com>
2022-10-04 13:58:08 -0700

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  numbers.forEach(number => sum += number);
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let product = 1;
+  numbers.forEach(number => product *= number);
+  return product;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let product = 1;
+  for(let i = 1; i <= number; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
