6999a59b4f9c82cf6bbaa83a5464c148678dc39c
Chris <chrisdnielson@gmail.com>
2022-10-04 16:57:11 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    //return the nth number in the fibonacci sequence
+    //the fibonacci sequence is defined as n = (n-1) + (n-2)
+    //parse input and initialize sequence
+    let n = parseInt(number);
+    let prev = 0;
+    let next = 1;
+    let temp = 0;
 
+    //edgecases
+    if(n < 0) {return "OOPS"};
+    if(n === 0) {return 0};
+    if(n === 1) {return 1};
+
+    for(let i = 2; i <= n; i++) { //counting from the start of the sequence, let i = 0; so the third number (and start of our sequence) is i[2]
+        temp = prev + next;
+        prev = next;
+        next = temp;
+    };
+
+    return temp;
 };
 
 // Do not edit below this line
